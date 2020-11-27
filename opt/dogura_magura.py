fffcaef66d6b79562724a82e7371c96e01ed4e05
suzuroku <y.suzumura0604@gmail.com>
2020-11-27 23:15:48 +0900

    型を追加

--- opt/dogura_magura.py
+++ opt/dogura_magura.py
@@ -10,18 +10,13 @@ import zipfile
 import re
 from janome.tokenizer import Tokenizer
 import collections
+from typing import Union
 
-# ドグラ・マグラ
-# URL = 'https://www.aozora.gr.jp/cards/000096/files/2093_ruby_28087.zip'
-# 蟹工船
-URL = 'https://www.aozora.gr.jp/cards/000156/files/1465_ruby_16804.zip'
-tmp_dir = 'tmp/'
-OUTPUT_FILE = 'output.txt'
 
-def download_file(url):
+def downloadFile(url: str, target_dir='./') -> Union[str, bool]:
     """URL を指定してカレントディレクトリにファイルをダウンロードする
     """
-    filename = url.split('/')[-1]
+    filename = target_dir + url.split('/')[-1]
     if os.path.exists(filename):
         # 負荷をかけないため
         print(filename, 'is downloaded already.')
@@ -38,8 +33,9 @@ def download_file(url):
     # ファイルが開けなかった場合は False を返す
     return False
 
-def unzip(filename, target_dir):
-    """ファイル名を指定して zip ファイルをカレントディレクトリに展開する
+
+def unzip(filename: str, target_dir='./') -> list:
+    """ファイル名を指定して zip ファイルを展開する
     """
     if not zipfile.is_zipfile(filename):
         print(filename, 'is not zipfile.')
@@ -47,28 +43,30 @@ def unzip(filename, target_dir):
     zfile = zipfile.ZipFile(filename)
     zfile.extractall(target_dir)
     zfile.close()
-    src_filenames = glob.glob(target_dir + '*.txt')
-    print(src_filenames)
-    return src_filenames
 
-def sjisToUtf8(path):
-    with open(path, mode="r", encoding='shift_jis') as f:
-        lines = f.readlines()
-        print(lines[:10])
+    # 展開したテキストのリストを取得する
+    dst_filenames = glob.glob(target_dir + '*.txt')
+    print(dst_filenames)
+    return dst_filenames
+
 
 def readSjis(path):
     with open(path, mode="r", encoding='shift_jis') as f:
-        lines = f.readlines()
-    return lines
+        text = f.read()
+    return text
 
 
-def convert(text):
+def preproccessing(text):
+    """テキストの前処理をする
     """
-    除去
-    参考: https://newtechnologylifestyle.net/711-2/
-    """
+    # 作品情報
+    lines = text.split('\n')
+    title = lines[0].strip()
+    author = lines[1].strip()
+    print(title, author)
+
     # ルビ、注釈などの除去
-    text  = re.split(r'\-{5,}', text)[2]
+    text = re.split(r'\-{5,}', text)[2]
     text = re.split(r'底本：', text)[0]
     text = re.sub(r'《.+?》', '', text)
     text = re.sub(r'［＃.+?］', '', text)
@@ -79,49 +77,40 @@ def convert(text):
     text = text.strip()
     return text
 
-def wakati(text):
-    # text = '\n'.join(text.split('\n')[:400])
-    words = {}
-    t = Tokenizer()
-    # print([ token for token in  t.tokenize(text[:200])])
 
+def wakati(text):
+    """テキストを分かち書きにして頻出単語を求める
+    """
+    t = Tokenizer()
     # 単語頻度
     # c = collections.Counter(t.tokenize(text, wakati=True))
 
     # 特定の品詞のみ
     c = collections.Counter(token.base_form for token in t.tokenize(text)
-                          if token.part_of_speech.startswith('名詞,固有名詞'))
-    # for token in t.tokenize(text):
-    #     word = token.surface
-    #     words[word] = words.get(word, 0) + 1
-    # d = [(v, k) for k, v in words.items()]
-    # d.sort()
-    # d.reverse()
+                            if token.part_of_speech.startswith('名詞,固有名詞'))
 
     # 降順で表示
     print(c.most_common()[:30])
 
-def main():
-    zip_file = download_file(URL)
-    filenames = unzip(zip_file, tmp_dir)
-    lines = readSjis(filenames[1])
-    print(lines[:10])
 
-    # 作品情報
-    title = lines[0].strip()
-    author = lines[1].strip()
-    print(title, author)
-    text = ''.join(lines[2:])
-    text = convert(text)
+def main():
+    # ドグラ・マグラ
+    URL = 'https://www.aozora.gr.jp/cards/000096/files/2093_ruby_28087.zip'
+    # 蟹工船
+    # URL = 'https://www.aozora.gr.jp/cards/000156/files/1465_ruby_16804.zip'
+    tmp_dir = 'tmp/'
+
+    zip_file = downloadFile(URL, tmp_dir)
+    filenames = unzip(zip_file, tmp_dir)
+    text = readSjis(filenames[0])
+    print(text[:100])
+
+    text = preproccessing(text)
     print(text.split('\n')[:40])
     print(text[:400])
 
     wakati(text)
 
 
-
 if __name__ == "__main__":
     main()
-
-
-    
