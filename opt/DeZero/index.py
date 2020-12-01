f36480fa27fb470ba1a0cdb89f98095a3e0333dc
suzuroku <y.suzumura0604@gmail.com>
2020-12-02 00:30:25 +0900

    バックプロバゲーションを追加

--- opt/DeZero/index.py
+++ opt/DeZero/index.py
@@ -4,6 +4,7 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
 
 
 class Function:
@@ -11,20 +12,34 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        self.input = input
         return output
     
     def forward(self, x):
+        raise NotImplementedError()
+        
+    def backward(self, gy):
         raise NotImplementedError()
 
 
 class Square(Function):
     def forward(self, x):
         return x ** 2
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
 
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+        
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
 if __name__ == "__main__":
     data = np.array(0.5)
@@ -39,3 +54,9 @@ if __name__ == "__main__":
     
     print(type(y))
     print(y.data)
+
+    y.grad = np.array(1.0)
+    b.grad = C.backward(y.grad)
+    a.grad = B.backward(b.grad)
+    x.grad = A.backward(a.grad)
+    print(x.grad)
