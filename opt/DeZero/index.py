b7a64293d35f779e7f93fce5f6dbfd84cc0e23f3
suzuroku <y.suzumura0604@gmail.com>
2020-12-02 00:23:12 +0900

    関数型を追加

--- opt/DeZero/index.py
+++ opt/DeZero/index.py
@@ -2,12 +2,36 @@ import numpy as np
 
 
 class Variable:
-    def __init__(self, data: np.array):
+    def __init__(self, data):
         self.data = data
 
 
-if __name__ == "__main__":
-    data = np.array(1.0)
-    x = Variable(data)
+class Function:
+    def __call__(self, input: Variable) -> Variable:
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
     
-    print(x.data)
+    def forward(self, x):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+if __name__ == "__main__":
+    data = np.array(10)
+    x = Variable(data)
+    a = Square()
+    b = Exp()
+    y = b(a(x))
+    
+    print(type(y))
+    print(y.data)
