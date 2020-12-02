b0d6ab58a177fcf83d412ea963f46ec8c288cbca
suzuroku <y.suzumura0604@gmail.com>
2020-12-03 00:13:08 +0900

    数値微分のテストを追加

--- opt/DeZero/test.py
+++ opt/DeZero/test.py
@@ -2,6 +2,14 @@ import unittest
 import dezero as dz
 import numpy as np
 
+def numerical_diff(f, x, eps=1e-4):
+    x0 = dz.Variable(x.data - eps)
+    x1 = dz.Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
 class SquareTest(unittest.TestCase):
     def test_forward(self):
         x = dz.Variable(np.array(2.0))
@@ -16,6 +24,14 @@ class SquareTest(unittest.TestCase):
         expected = np.array(6.0)
         self.assertEqual(x.grad, expected)
 
+    def test_gradient_check(self):
+        x = dz.Variable(np.random.rand(1))
+        y = dz.square(x)
+        y.backward()
+        num_grad = numerical_diff(dz.square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+
 class ExeTest(unittest.TestCase):
     def test_forward(self):
         x = dz.Variable(np.array(2.0))
@@ -29,5 +45,13 @@ class ExeTest(unittest.TestCase):
         y.backward()
         expected = np.exp(2.0)
         self.assertEqual(x.grad, expected)
+        
+    def test_gradient_check(self):
+        x = dz.Variable(np.random.rand(1))
+        y = dz.square(x)
+        y.backward()
+        num_grad = numerical_diff(dz.square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
 
 unittest.main()
