efdcaaa10cba91ed2922508cfd586d6cae06baf9
suzuroku <y.suzumura0604@gmail.com>
2020-12-03 00:03:10 +0900

    squareのbackwardのテスト

--- opt/DeZero/test.py
+++ opt/DeZero/test.py
@@ -8,6 +8,13 @@ class SquareTest(unittest.TestCase):
         y = dz.square(x)
         expected = np.array(4.0)
         self.assertEqual(y.data, expected)
+    
+    def test_backward(self):
+        x = dz.Variable(np.array(3.0))
+        y = dz.square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
 
 
 
