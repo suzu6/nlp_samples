95a6d774803a4b3b2ed1e757cb9caf482bff6225
suzuroku <y.suzumura0604@gmail.com>
2020-12-04 00:09:41 +0900

    メモリ開放

--- opt/DeZero/dezero.py
+++ opt/DeZero/dezero.py
@@ -1,4 +1,24 @@
+import weakref
 import numpy as np
+import contextlib
+
+
+class Config:
+    enable_backprop = True
+
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+def no_grad():
+    return using_config('enable_backprop', False)
 
 
 class Variable:
@@ -11,12 +31,15 @@ class Variable:
         self.grad = None
         self.creator = None
         self.generation = 0
-    
+
     def set_creator(self, func):
         self.creator = func
         self.generation = func.generation + 1
 
-    def backward(self):
+    def cleargrad(self):
+        self.grad = None
+
+    def backward(self, retain_grad=False):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
@@ -28,16 +51,16 @@ class Variable:
                 funcs.append(f)
                 seen_set.add(f)
                 funcs.sort(key=lambda x: x.generation)
-        
+
         add_func(self.creator)
 
         while funcs:
             f = funcs.pop()
-            gys = [output.grad for output in f.outputs]
+            gys = [output().grad for output in f.outputs]  # output is weakref
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
                 gxs = (gxs,)
-            
+
             for x, gx in zip(f.inputs, gxs):
                 if x.grad is None:
                     x.grad = gx
@@ -46,14 +69,17 @@ class Variable:
 
                 if x.creator is not None:
                     add_func(x.creator)
-    
-    def cleargrad(self):
-        self.grad = None
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None  # y is weakref
+
 
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
     return x
+
 
 class Function:
     def __call__(self, *inputs):
@@ -63,18 +89,35 @@ class Function:
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
 
-        self.generation = max([x.generation for x in inputs])
-        for output in outputs:
-            output.set_creator(self)
-        self.inputs = inputs
-        self.outputs = outputs
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+
         return outputs if len(outputs) > 1 else outputs[0]
-    
+
     def forward(self, xs):
         raise NotImplementedError()
-        
+
     def backward(self, gys):
         raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+
+def square(x):
+    return Square()(x)
 
 
 class Add(Function):
@@ -85,38 +128,24 @@ class Add(Function):
     def backward(self, gy):
         return gy, gy
 
-class Square(Function):
-    def forward(self, x):
-        return x ** 2
-    
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = 2 * x * gy
-        return gx
-
-
-class Exp(Function):
-    def forward(self, x):
-        return np.exp(x)
-        
-    def backward(self, gy):
-        x = self.inputs.data
-        gx = np.exp(x) * gy
-        return gx
 
 def add(x0, x1):
     return Add()(x0, x1)
 
-def square(x):
-    return Square()(x)
-
-def exp(x):
-    return Exp()(x)
-
 if __name__ == "__main__":
-    x = Variable(np.array(2.0))
-    a = square(x)
-    y = add(square(a), square(a))
+    x0 = Variable(np.array(1.0))
+    x1 = Variable(np.array(1.0))
+    t = add(x0, x1)
+    y = add(x0, t)
     y.backward()
-    print(y.data)
-    print(x.grad)
+    print(y.grad, t.grad)  # None None
+    print(x0.grad, x1.grad)  # 2.0 1.0
+
+
+    with using_config('enable_backprop', False):
+        x = Variable(np.array(2.0))
+        y = square(x)
+
+    with no_grad():
+        x = Variable(np.array(2.0))
+        y = square(x)
