2512298c19761e5039f597e3a14bcbddd3481fa0
suzuroku <y.suzumura0604@gmail.com>
2020-12-26 21:02:43 +0900

    19.1 変数に名前を付けた

--- opt/DeZero/dezero.py
+++ opt/DeZero/dezero.py
@@ -22,12 +22,13 @@ def no_grad():
 
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} is not supported'.format(type(data)))
 
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
